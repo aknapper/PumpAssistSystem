2fbebbf0dffad25b991e651773873ebb9deef383
Aaron Knapper <34011307+aknapper@users.noreply.github.com>
2022-01-27 00:43:43 -0500

    create HPP_Controller class

---
+++ lib/pump-assist/HPP_Controller.h
@@ -0,0 +1,15 @@
+class HPP_Controller 
+{
+public:
+    // code
+    int var1;
+    int var2;
+    int var3;
+    HPP_Controller(int x, int y, int z);
+
+protected:
+    // code
+
+private:
+    // code
+};
---
+++ lib/pump-assist/HPP_Controlller.cpp
@@ -0,0 +1,8 @@
+#include <HPP_Controller.h>
+
+HPP_Controller::HPP_Controller(int x, int y, int z)
+{
+    var1 = x;
+    var2 = y;
+    var3 = z;
+}
